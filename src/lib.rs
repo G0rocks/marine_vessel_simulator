101f72f86ec8624fbc6c869786b9a9072f88e5dd
G0rocks <huldarh@gmail.com>
2025-04-14 16:57:59 +0000

    lib connected to main

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+/// Marine vessel simulator simulates the behaviour of marine vessels out at sea.
+/// Author: G0rocks
+/// Date: 2025-04-14
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+/// Here the marine vessel simulator is tested and implemented by examples.
+
+use marine_vessel_simulator::add; // Import the add function from the marine_vessel_simulator crate
+
+fn main() {
+
+    let x = add(2, 2);
+    // Print the result to the console
+    println!("The result of adding 2 and 2 is: {}", x);
+
+    // Print text to the console
+    println!("Hello World!");
+}
